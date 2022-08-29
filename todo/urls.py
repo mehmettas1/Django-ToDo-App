d84bde4bb326bf24882f89a0a47359c1d30ccd73
mehmet <tas349873@gmail.com>
2022-08-29 22:32:38 +0300

    update

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home,todo_create
+from .views import home, todo_create, todo_update
 
 urlpatterns = [
-    path("",home,name="home"),
-    path("add/",todo_create,name="add"),
+    path('', home, name="home"),
+    path('add/', todo_create, name="add"),
+    path('update/<int:id>', todo_update, name="update"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,4 @@
+import re
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Todo
@@ -6,35 +7,41 @@ from .forms import TodoForm
 def home(request):
     todos = Todo.objects.all()
     form = TodoForm()
+    
     context = {
         "todos" : todos,
         "form" : form
     }
-
     return render(request, "todo/home.html", context)
+
 
 def todo_create(request):
     form = TodoForm()
-
+    
     if request.method == "POST":
         form = TodoForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("home")
-
+    
     context = {
         "form" : form
     }
-
     return render(request, "todo/todo_add.html", context)
 
 
-def todo_update(request,id):
-    todo =Todo.objects.get(id=id)
+def todo_update(request, id):
+    todo = Todo.objects.get(id=id)
     form = TodoForm(instance=todo)
-
-    context={
-        "todo":todo,
-        "form":form
+    
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    
+    context = {
+        "todo" : todo,
+        "form" : form
     }
-    return render(request,"todo/todo_update.html",context)
+    return render(request, "todo/todo_update.html", context)
