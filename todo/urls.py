e7675877e108c593b1befe673c41c54ae58132b0
mehmet <tas349873@gmail.com>
2022-08-29 21:21:21 +0300

    update

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home,todo_create
 
 urlpatterns = [
     path("",home,name="home"),
+    path("add/",todo_create,name="add"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Todo
+from .forms import TodoForm
 def home(request):
-    return HttpResponse("welcome")
+    todos = Todo.objects.all()
+    context = {
+        "todos" : todos
+    }
+
+    return render(request, "todo/home.html", context)
+
+def todo_create(request):
+    form = TodoForm()
+    if request.method= "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+    
+    context = {
+        "form" : form
+    }
+    return render(request, "todo/todo_add.html", context)
